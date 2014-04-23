d47f7196f4ced497c0f85b1a9cbfca5198fa9deb
Markus Witt <feuerrot@feuerrot.org>
2014-04-23 22:35:13 +0200

    even more color

--- main.py
+++ main.py
@@ -3,11 +3,19 @@
 import socket
 import sys
 
+xstart = 0
+xstop  = 600
+xstep  = 1
+ystart = 0
+ystop  = 600
+ystep  = 1
+
 
 host = sys.argv[1]
 port = int(sys.argv[2])
 
-string = 'R3K'
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host,port))
 
 def text(x, y, t):
 	return 'TEXT {} {} {}\n'.format(x, y, t).encode('UTF-8')
@@ -21,7 +29,10 @@ def makecon(text):
 	s.sendall(text)
 	s.close()
 
+def send(text):
+	sock.sendall(text)
+
 while True:
-	for x in range(100, 800):
-		for y in range(100, 600):
-			makecon(pixl(x, y, x%255, y%255, (x+y)%255))
+	for x in range(xstart, xstop, xstep):
+		for y in range(ystart, ystop, ystep):
+			send(pixl(x, y, x%255, y%255, 255-(x-y)%255))
