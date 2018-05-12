5faa94cf1113fba50cb5240bcbc1ac6237ab20a0
Markus Witt <feuerrot@feuerrot.org>
2018-05-12 22:28:22 +0200

    thread

--- thread.py
+++ thread.py
@@ -6,6 +6,9 @@ import threading
 import time
 from PIL import Image
 
+NUMOFTHREADS = 8
+ALPHA = 0
+
 xstart = None
 xstop  = 192
 xstep  = 1
@@ -17,8 +20,15 @@ host = sys.argv[1]
 port = int(sys.argv[2])
 image = str(sys.argv[3])
 try:
-	xstart = int(sys.argv[4])
-	ystart = int(sys.argv[5])
+	sip = str(sys.argv[4])
+except:
+	sip = None
+
+try:
+	x = int(sys.argv[5])
+	y = int(sys.argv[6])
+	xstart = x
+	ystart = y
 except:
 	pass
 
@@ -34,7 +44,18 @@ class pixelsender(threading.Thread):
 	
 	def connect(self):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.connect((host,port))
+		if sip != None:
+			self.sock.bind((sip, 0))
+		while True:
+			try:
+				self.sock.connect((host,port))
+				break
+			except TimeoutError:
+				sys.stdout.write('T')
+				sys.stdout.flush()
+				time.sleep(1)
+		sys.stdout.write('c')
+		sys.stdout.flush()
 
 	def run(self):
 		self.addpixel()
@@ -45,7 +66,7 @@ class pixelsender(threading.Thread):
 			try:
 				for elem in self.cmd:
 					subcmd.append(elem)
-					if len(subcmd) > 65*100:
+					if len(subcmd) > 2**10:
 						self.sock.sendall(b"".join(subcmd))
 						subcmd = []
 			except BrokenPipeError:
@@ -71,12 +92,21 @@ def command_pixel(x, y, r, g, b):
 def getsize(s):
 	s.sendall('SIZE\n'.encode('UTF-8'))
 	msg = s.recv(64).decode('UTF-8')
-	(_, xsize, ysize) = msg.strip().split()
+	size = msg.strip().split()
+	print(len(size))
+	if len(size) == 2:
+		(xsize, ysize) = size
+	elif len(size) == 3:
+		(_, xsize, ysize) = size
+	elif len(size) == 4:
+		(_, xsize, ysize, _) = size
 	print('Size: {}x{} px'.format(xsize,ysize))
 	return (int(xsize), int(ysize))
 
 # Get the size of the image
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	if sip != None:
+		s.bind((sip, 0))
 	s.connect((host,port))
 	(xstop, ystop) = getsize(s)
 
@@ -86,21 +116,23 @@ def addpixel():
 	print('Img:  {}x{} px'.format(ix, iy))
 	print('addpixel')
 	if xstart == None or ystart == None:
-		xstart = xstop/2 - ix/2
-		ystart = ystop/2 - iy/2
+		xstart = xstop - ix
+		ystart = ystop - iy
 	print('xstart: {}\nystart: {}'.format(xstart, ystart))
 	for x in range(ix):
 		for y in range(iy):
 			(r, g, b) = img.getpixel((x,y))
+			if ALPHA - 2 <= r and r <= ALPHA + 2 and ALPHA - 2 <= g and g <= ALPHA + 2 and ALPHA - 2 <= b and b <= ALPHA + 2:
+				continue
 			commands.append(command_pixel(x+xstart, y+ystart, r, g, b))
 	print('addedpixels')
 
 try:
 	addpixel()
-	threads.append(pixelsender(host, port))
-	threads.append(pixelsender(host, port))
-	threads.append(pixelsender(host, port))
-	threads.append(pixelsender(host, port))
+	for _ in range(NUMOFTHREADS):
+		threads.append(pixelsender(host, port))
+		sys.stdout.write('t')
+		sys.stdout.flush()
 
 	for elem in threads:
 		elem.daemon = True
